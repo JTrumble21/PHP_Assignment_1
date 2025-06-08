237fd3d2f08aca145fb955b237d398e9fbcdce83
JTrumble21 <jessi.oostrom@triosstudent.com>
2025-06-08 02:58:35 -0400

    debugging log in

--- login.php
+++ login.php
@@ -1,52 +1,51 @@
 <?php
-    session_start();
+session_start();
 
-    // get data from the form
-    $user_name = filter_input(INPUT_POST, 'user_name');    
-    $password = filter_input(INPUT_POST, 'password');
+// get data from the form
+$user_name = filter_input(INPUT_POST, 'user_name');    
+$password = filter_input(INPUT_POST, 'password');
 
-    require_once('database.php');
+require_once('database.php');
 
-    $query = 'SELECT password FROM users
-                WHERE userName = :userName';
-    $statement1 = $db->prepare($query);
+$query = 'SELECT password FROM users WHERE userName = :userName';
+$statement1 = $db->prepare($query);
 
-    $statement1->bindValue(':userName', $user_name);
+$statement1->bindValue(':userName', $user_name);
 
-    $statement1->execute();
-    $row = $statement1->fetch();    
+$statement1->execute();
+$row = $statement1->fetch();    
 
-    $statement1->closeCursor();
+$statement1->closeCursor();
 
-    if (!$row) 
-    {
-        $_SESSION = [];
-        session_destroy();
+if (!$row) 
+{
+    $_SESSION = [];
+    session_destroy();
 
-      $url = "/PHP_Assignment_1/login_confirmation.php";
-      header("Location: " . $url);
-      die();
-    }
+    $url = "/PHP_Assignment_1/login_form.php";  // Redirect back to login form
+    header("Location: " . $url);
+    die();
+}
 
-    $hash = $row['password'];
+$hash = $row['password'];
 
-    $_SESSION["isLoggedIn"] = password_verify($password, $hash);
+$_SESSION["isLoggedIn"] = password_verify($password, $hash);
 
-    if ($_SESSION["isLoggedIn"] == TRUE)
-    {
-        $_SESSION["userName"] = $user_name;
+if ($_SESSION["isLoggedIn"] == TRUE)
+{
+    $_SESSION["userName"] = $user_name;
 
-      $url = "/PHP_Assignment_1/login_confirmation.php";
-      header("Location: " . $url);
-      die();
-    }
-    elseif ($_SESSION["isLoggedIn"] == FALSE)
-    {
-        $_SESSION = [];
-        session_destroy();
+    $url = "/PHP_Assignment_1/login_confirmation.php";
+    header("Location: " . $url);
+    die();
+}
+elseif ($_SESSION["isLoggedIn"] == FALSE)
+{
+    $_SESSION = [];
+    session_destroy();
 
-      $url = "/PHP_Assignment_1/login_form.php";
-      header("Location: " . $url);
-      die();
-    }
+    $url = "/PHP_Assignment_1/login_form.php";
+    header("Location: " . $url);
+    die();
+}
 ?>
