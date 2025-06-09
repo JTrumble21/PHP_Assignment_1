0e9ec76bd9c85caa06e60bf63741a58fdfaa87a7
JTrumble21 <jessi.oostrom@triosstudent.com>
2025-06-09 01:25:58 -0400

    debug login

--- login.php
+++ login.php
@@ -1,4 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
+// rest of your code...
+
+
+<?php
 session_start();
 
 $user_name = filter_input(INPUT_POST, 'username');    
