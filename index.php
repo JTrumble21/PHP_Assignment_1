ed161c9da2ad21edf228b82ba2525ea52e8c648c
JTrumble21 <jessi.oostrom@triosstudent.com>
2025-06-09 02:31:21 -0400

    update index.php

--- index.php
+++ index.php
@@ -39,6 +39,7 @@ $cars = $db->query("SELECT * FROM cars ORDER BY year DESC")->fetchAll();
           <td>
             <a href="edit_vehicle.php?id=<?= $car['id'] ?>">Edit</a> |
             <a href="delete_vehicle.php?id=<?= $car['id'] ?>" onclick="return confirm('Delete this vehicle?')">Delete</a>
+            <a href="logout.php" class="logout-button">Logout</a>
           </td>
         </tr>
       <?php endforeach; ?>
