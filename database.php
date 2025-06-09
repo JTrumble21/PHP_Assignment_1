6d8900ba57692cfbe0a937075ea53af3ebdbb43f
JTrumble21 <jessi.oostrom@triosstudent.com>
2025-06-09 02:05:42 -0400

    typo in database.php

--- database.php
+++ database.php
@@ -5,7 +5,9 @@ $password = '';
 
 try {
     $db = new PDO($dsn, $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
+    session_start();
     $_SESSION["database_error"] = $e->getMessage();
     header("Location: database_error.php");
     exit();
