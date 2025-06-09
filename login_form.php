8abfc29f21f176b2ec12a96b730f63423012e789
JTrumble21 <jessi.oostrom@triosstudent.com>
2025-06-09 02:46:58 -0400

    edited for updated css

--- login_form.php
+++ login_form.php
@@ -8,7 +8,7 @@ unset($_SESSION['login_error']);
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="css/main.css">
+   <link rel="stylesheet" href="css/main.css?v=2">
 </head>
 <body>
     <main>
