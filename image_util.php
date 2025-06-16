60cb9cef0564386f3b4d810c3742e5997389cf86
JTrumble21 <jessi.oostrom@triosstudent.com>
2025-06-16 01:52:57 -0400

    updated image_util

--- image_util.php
+++ image_util.php
@@ -9,13 +9,6 @@ function process_image($dir, $filename) {
     $image_path_400 = $dir . $image_name . '_400' . $ext;
     $image_path_100 = $dir . $image_name . '_100' . $ext;
 
-    echo "Original: " . realpath($image_path) . "\n";
-    if (resize_image($image_path, $image_path_400, 400, 300)) {
-        echo "Saved resized 400px image to: $image_path_400\n";
-    }
-    if (resize_image($image_path, $image_path_100, 100, 100)) {
-        echo "Saved resized 100px image to: $image_path_100\n";
-    }
 }
 
 function resize_image($old_image_path, $new_image_path, $max_width, $max_height) {
