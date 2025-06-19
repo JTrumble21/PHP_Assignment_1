ef065275a9c8c134644f0ae7277a0c0883d6cdee
JTrumble21 <jessi.oostrom@triosstudent.com>
2025-06-19 08:09:38 -0400

    edited typo in css

--- login_form.php
+++ login_form.php
@@ -8,7 +8,7 @@ unset($_SESSION['login_error']);
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="css/main.css?v=2">
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body class="login-page">
     <main>
